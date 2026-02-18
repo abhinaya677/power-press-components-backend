7ca060f853713de5597176c1c8375f7854ef0fe9
abhinaya677 <nabhi5423@gmail.com>
2026-02-18 20:30:15 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,22 @@
+require('dotenv').config();  // load .env
 const mysql = require("mysql2");
-require("dotenv").config();
 
+
+// create a connection pool
 const db = mysql.createPool(process.env.MYSQL_URL);
 
+// test connection
 db.getConnection((err, connection) => {
   if (err) {
-    console.error("❌ Database connection failed:", err);
-  } else {
-    console.log("✅ Connected to Railway MySQL");
-    connection.release();
+    console.error("Database connection failed:", err);
+    return;
   }
+  console.log("✅ Connected to MySQL!");
+  connection.release();
 });
 
-module.exports = db;
+module.exports = db;  // export for use in other files
+
+
+
+
