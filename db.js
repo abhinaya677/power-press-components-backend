0105223b062ff619659e137b1ca1a67844b45b27
abhinaya677 <nabhi5423@gmail.com>
2026-02-18 23:18:06 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Create a pool for better performance
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -13,5 +12,4 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
-// Export promise-based pool for async/await
 module.exports = pool.promise();
