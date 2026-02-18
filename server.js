1dc3a987f67697d99a54158ce44df10b80f4803c
abhinaya677 <nabhi5423@gmail.com>
2026-02-18 23:21:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -87,12 +87,8 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 
 
 
